a7a29da02deddbaa72fa974d8df93186394323d8
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2025-04-11 19:16:10 -0300

    Update provider.tf

--- terraform/database/provider.tf
+++ terraform/database/provider.tf
@@ -17,12 +17,6 @@ terraform {
 }
 
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = "us-east-1"
-}
-
-provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = var.aws_region
