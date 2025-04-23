49c6abcec3d3c1e0e336fff8c0007ff38048804f
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2025-04-22 23:09:47 -0300

    Update s3.tf

--- terraform/s3/s3.tf
+++ terraform/s3/s3.tf
@@ -1,7 +1,4 @@
-resource "aws_s3_bucket" "video_processor_techchalenge_bucket" {
-  bucket = "video-processor-techchalenge-bucket"
+resource "aws_s3_bucket" "processed_bucket" {
+  bucket = "processed-bucket"
 
-  tags = {
-    Name = "video-processor-S3"
-  }
 }
