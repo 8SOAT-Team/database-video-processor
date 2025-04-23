a2cd2049ab671059b886b11623fd28aad469aab9
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2025-04-22 23:28:31 -0300

    Update s3.tf

--- terraform/s3/s3.tf
+++ terraform/s3/s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "processed_bucket" {
-  bucket = "processed-bucket"
+resource "aws_s3_bucket" "processed_bucket_techchallenge" {
+  bucket = "processed-bucket-techchallenge"
 
 }
