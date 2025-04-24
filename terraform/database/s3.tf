cd577e6103b78fd4a778619d898e2555f845d4e4
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2025-04-23 22:26:32 -0300

    Update s3.tf

--- terraform/database/s3.tf
+++ terraform/database/s3.tf
@@ -1,4 +1,11 @@
 resource "aws_s3_bucket" "processed_bucket_techchallenge" {
   bucket = "processed-bucket-techchallenge"
 
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
 }
