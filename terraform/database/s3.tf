1f2201d38021f99b89697b8c71e348a286cfab91
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2025-04-25 01:38:03 -0300

    Update s3.tf

--- terraform/database/s3.tf
+++ terraform/database/s3.tf
@@ -9,3 +9,29 @@ resource "aws_s3_bucket" "processed_bucket_techchallenge" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket_policy" "processed_bucket_policy" {
+  bucket = aws_s3_bucket.processed_bucket_techchallenge.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid: "AllowIRSAAccess",
+        Effect: "Allow",
+        Principal: {
+          AWS: "arn:aws:iam::585008076257:role/notificacao-api-irsa-role"
+        },
+        Action: [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ],
+        Resource: [
+          "arn:aws:s3:::${aws_s3_bucket.processed_bucket_techchallenge.bucket}",
+          "arn:aws:s3:::${aws_s3_bucket.processed_bucket_techchallenge.bucket}/*"
+        ]
+      }
+    ]
+  })
+}
